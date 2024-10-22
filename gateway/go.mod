167b5368a3dc68d6c9aa6ca5d15f4228fdb3c80b
Alvintan0712 <tjan2000@gmail.com>
2024-10-22 15:28:07 +0800

    add godotenv to read the dotenv file

--- gateway/go.mod
+++ gateway/go.mod
@@ -1,3 +1,5 @@
 module example.com/oms/gateway
 
 go 1.23.2
+
+require github.com/joho/godotenv v1.5.1
--- gateway/main.go
+++ gateway/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"example.com/oms/common"
+	_ "github.com/joho/godotenv/autoload"
 )
 
-const (
-	addr = ":8080"
+var (
+	addr = common.EnvString("HTTP_ADDR", ":8080")
 )
 
 func main() {
