6e72cda64eccd73a718c84e808580ef78abb8860
Alvintan0712 <tjan2000@gmail.com>
2024-11-21 21:44:50 +0800

    add kafka in docker compose

--- stock/main.go
+++ stock/main.go
@@ -6,11 +6,13 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"example.com/oms/common"
 	"example.com/oms/common/discovery"
 	"example.com/oms/common/discovery/consul"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	_ "github.com/joho/godotenv/autoload"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
@@ -22,8 +24,103 @@ var (
 	servicePort = common.EnvString("SERVICE_PORT", "8083")
 	serviceName = common.EnvString("SERVICE_NAME", "stock-service")
 
+	kafkaBrokers         = common.EnvString("KAFKA_BROKERS", "localhost:9092")
+	kafkaConsumerGroupId = common.EnvString("KAFKA_CONSUMER_GROUP_ID", "stock-consumer-group")
+	kafkaTopics          = common.EnvString("KAFKA_TOPICS", "")
+
 	debug = common.EnvString("DEBUG", "false") == "true"
 )
+
+func checkTopicExists(adminClient *kafka.AdminClient, topic string) bool {
+	for {
+		metadata, err := adminClient.GetMetadata(&topic, false, 5000)
+		if err != nil {
+			log.Printf("Error fetching metadata: %v\n", err)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		for t := range metadata.Topics {
+			fmt.Println("topic:", t)
+			if t == topic {
+				return true
+			}
+		}
+
+		break
+	}
+
+	return false
+}
+
+func setupKafkaTopics(adminClient *kafka.AdminClient) error {
+	topics := strings.Split(kafkaTopics, ",")
+
+	topicSpecifications := []kafka.TopicSpecification{}
+	for _, topic := range topics {
+		if checkTopicExists(adminClient, topic) {
+			log.Println("topic", topic, "exists")
+			continue
+		}
+
+		topicSpecifications = append(topicSpecifications, kafka.TopicSpecification{
+			Topic:             topic,
+			NumPartitions:     3,
+			ReplicationFactor: 3,
+		})
+	}
+
+	log.Println("topics specification:", topicSpecifications)
+
+	if len(topicSpecifications) > 0 {
+		_, err := adminClient.CreateTopics(context.Background(), topicSpecifications)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func initialKafka(adminClient *kafka.AdminClient) error {
+	return setupKafkaTopics(adminClient)
+}
+
+func startKafkaConsumer() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  kafkaBrokers,
+		"group.id":           kafkaConsumerGroupId,
+		"auto.offset.reset":  "earliest",
+		"enable.auto.commit": false,
+	})
+	if err != nil {
+		log.Printf("failed to create Kafka consumer: %v\n", err)
+		return
+	}
+
+	topics := strings.Split(kafkaTopics, ",")
+	err = consumer.SubscribeTopics(topics, nil)
+	if err != nil {
+		log.Printf("failed to subscribe to topics: %v\n", err)
+		return
+	}
+
+	fmt.Println("Kafka consumer started...")
+	for {
+		msg, err := consumer.ReadMessage(100 * time.Millisecond)
+		if err != nil {
+			log.Printf("failed to read message: %v\n", err)
+			continue
+		}
+
+		// consume event (replace it to other function)
+		fmt.Printf("Consumed event from topic: %s: key = %-10s value = %s\n", *msg.TopicPartition.Topic, string(msg.Key), string(msg.Value))
+		_, err = consumer.CommitMessage(msg)
+		if err != nil {
+			log.Printf("failed to commit message: %v\n", err)
+		}
+	}
+}
 
 func main() {
 	ctx := context.Background()
@@ -57,6 +154,20 @@ func main() {
 			time.Sleep(time.Second)
 		}
 	}()
+
+	// start kafka consumer
+	adminClient, err := kafka.NewAdminClient(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaBrokers,
+	})
+	if err != nil {
+		log.Printf("failed to create admin client: %s\n", err)
+	} else {
+		if err := initialKafka(adminClient); err != nil {
+			log.Printf("error in initial Kafka: %v\n", err)
+		} else {
+			go startKafkaConsumer()
+		}
+	}
 
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%s", serviceHost, servicePort))
 	if err != nil {
